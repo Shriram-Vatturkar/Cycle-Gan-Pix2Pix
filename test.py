dccae030297a159d3c445a885f10a986f40cf223
Tanya710 <139198121+Tanya710@users.noreply.github.com>
2025-04-14 16:05:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -32,49 +32,90 @@ from data import create_dataset
 from models import create_model
 from util.visualizer import save_images
 from util import html
+import torch
+from torch.nn.parallel import DataParallel
+from pytorch_fid import fid_score
+from tqdm import tqdm  # for progress bar
 
 try:
     import wandb
 except ImportError:
     print('Warning: wandb package cannot be found. The option "--use_wandb" will result in error.')
 
-
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
-    # hard-code some parameters for test
-    opt.num_threads = 0   # test code only supports num_threads = 0
-    opt.batch_size = 1    # test code only supports batch_size = 1
-    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
-    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
 
-    # initialize logger
+    # ========== Fixed Testing Setup ==========
+    opt.num_threads = 0
+    opt.batch_size = 1
+    opt.serial_batches = True
+    opt.no_flip = True
+    opt.display_id = -1
+
+    # ========== Multi-GPU Setup ==========
+    if torch.cuda.device_count() > 1:
+        opt.batch_size = torch.cuda.device_count() * 4
+        opt.serial_batches = False
+
+    dataset = create_dataset(opt)
+    model = create_model(opt)
+    model.setup(opt)
+
+    if torch.cuda.device_count() > 1:
+        model.netG = DataParallel(model.netG)
+
+    # initialize wandb
     if opt.use_wandb:
         wandb_run = wandb.init(project=opt.wandb_project_name, name=opt.name, config=opt) if not wandb.run else wandb.run
         wandb_run._label(repo='CycleGAN-and-pix2pix')
 
-    # create a website
-    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
-    if opt.load_iter > 0:  # load_iter is 0 by default
-        web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
-    print('creating web directory', web_dir)
-    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
-    # test with eval mode. This only affects layers like batchnorm and dropout.
-    # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
-    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
+    # ========== Setup HTML Webpage ==========
+    web_dir = os.path.join(opt.results_dir, opt.name, f'{opt.phase}_{opt.epoch}')
+    if opt.load_iter > 0:
+        web_dir = f'{web_dir}_iter{opt.load_iter}'
+    print('Creating web directory:', web_dir)
+    webpage = html.HTML(web_dir, f'Experiment = {opt.name}, Phase = {opt.phase}, Epoch = {opt.epoch}')
+
+    # ========== Setup Directories for FID ==========
+    real_images_path = os.path.join(web_dir, 'real_images')
+    fake_images_path = os.path.join(web_dir, 'fake_images')
+    os.makedirs(real_images_path, exist_ok=True)
+    os.makedirs(fake_images_path, exist_ok=True)
+
+    # ========== Set model to evaluation mode if required ==========
     if opt.eval:
         model.eval()
-    for i, data in enumerate(dataset):
-        if i >= opt.num_test:  # only apply our model to opt.num_test images.
+
+    for i, data in enumerate(tqdm(dataset, desc="Processing images")):
+        if i >= opt.num_test:
             break
-        model.set_input(data)  # unpack data from data loader
-        model.test()           # run inference
-        visuals = model.get_current_visuals()  # get image results
-        img_path = model.get_image_paths()     # get image paths
-        if i % 5 == 0:  # save images to an HTML file
-            print('processing (%04d)-th image... %s' % (i, img_path))
-        save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize, use_wandb=opt.use_wandb)
-    webpage.save()  # save the HTML
+
+        model.set_input(data)
+        with torch.no_grad():
+            model.test()
+
+        visuals = model.get_current_visuals()
+        img_path = model.get_image_paths()
+
+        if i % 5 == 0:
+            print(f'Processing ({i:04d})-th image: {img_path}')
+
+        save_images(
+            webpage, visuals, img_path,
+            aspect_ratio=opt.aspect_ratio,
+            width=opt.display_winsize,
+            use_wandb=opt.use_wandb,
+            real_path=real_images_path,
+            fake_path=fake_images_path
+        )
+
+    webpage.save()  # Save HTML page
+
+    # ========== FID Calculation ==========
+    fid_value = fid_score.calculate_fid_given_paths(
+        [real_images_path, fake_images_path],
+        batch_size=opt.batch_size,
+        device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+        dims=2048
+    )
+    print(f'\nFID Score: {fid_value:.2f}')
